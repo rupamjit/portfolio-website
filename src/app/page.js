41826dc308cd1ef5a7ac0c01e26d3e8e154768b6
Rupamjit Ghosh <rupamjitghosh@Rupamjits-MacBook-Air.local>
2025-02-22 23:49:27 +0530

    add navBar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col  bg-[#121212] container mx-auto py-4 px-12">
-      <HeroSection/>
-    </div>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+      
+      </div>
+     
+    </main>
   );
 }
