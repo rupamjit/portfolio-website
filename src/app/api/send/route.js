81b69543d18fa4b72f96bc792c21ff777030331b
Rupamjit Ghosh <rupamjitghosh@Rupamjits-MacBook-Air.local>
2025-02-23 13:55:49 +0530

    add nodemailer

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,28 +1,35 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
+
   try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
-    });
-    return NextResponse.json(data);
+    const mailOptions = {
+      from: process.env.SMTP_USER, 
+      to: process.env.SMTP_USER, 
+      subject: `Subject: ${subject}`, 
+      text: `Message from ${email}:\n\n${message}`,
+      replyTo: email,
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+
+    return NextResponse.json({ message: "Email sent successfully" });
   } catch (error) {
-    return NextResponse.json({ error });
+    console.error("Error sending email:", error);
+    return NextResponse.json({ error: error.message });
   }
 }
