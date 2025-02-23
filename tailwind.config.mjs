56c9dcb2966e3608ff85f5b29ae5742deeef26b9
Rupamjit Ghosh <rupamjitghosh@Rupamjits-MacBook-Air.local>
2025-02-23 15:33:19 +0530

    update gradient color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,8 +16,8 @@ module.exports = {
     },
     colors: {
       ...colors,
-      primary: colors.purple,
-      secondary: colors.yellow,
+      primary: colors.blue,
+      secondary: colors.orange,
     },
   },
   plugins: [],
